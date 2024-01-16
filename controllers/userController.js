1c875885019323d6bda08e1ae0f5a928729801ce
irfan usuf <irfanusuf33@gmail.com>
2024-01-16 13:41:39 +0530

    v1.2

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,66 +1,65 @@
-const User = require('../models/userModel')
-const bcrypt = require('bcrypt')
+const User = require("../models/userModel");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
+//register function whenever this function will be called it will accept payload in json username ,email......
+//password and save that data in mongo document User
 
 const registerController = async (req, res) => {
-
-
   try {
-    const { username, email, password } = req.body
-    const existingUser = await User.findOne({ email })
-
-
-    
-
-    if (username !== "") {
+    const { username, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (username || email || password !== "") {
       if (!existingUser) {
-
-
-
-        const hashedPassword = await bcrypt.hash(password, 10)
-        const newUser = new User({ username, email, password: hashedPassword })
-        await newUser.save()
-        res.status(201).json({ message: "user created" })
-
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const newUser = new User({ username, email, password: hashedPassword });
+        await newUser.save();
+        res.status(201).json({ message: "user created" });
+      } else {
+        res.json({ message: "user Already Exits" });
       }
-      else {
+    } else {
+      res.status(401).json({ message: "All credentials Required" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
 
-        res.json({ message: "user Already Exits" })
+const loginController = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const isUser = await User.findOne({ username });
+    if (username !== "" && password !== "") {
+      if (isUser) {
+        const passVerify = await bcrypt.compare(password, isUser.password);
+        if (passVerify) {
+
+          const token = await jwt.sign({appUser : isUser.username} , 'sevenSprings')
+          res.status(200).json({ message: "Logged In" } , token);
+        } else {
+          res.json({ message: "Password Doesnot Match" });
+        }
+      } else {
+        res.json({ message: "User Not Found" });
       }
-
+    } else {
+      res.json({ message: "All Credentials Required" });
     }
-
-    else {
-      res.status(401).json({ message: "All credentials Required" })
-    }
-
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
   }
-  catch (error) {
-    console.log(error)
-    res.status(500).json({ message: "Server Error" })
-  }
+};
 
-}
+const logoutController = (req, res) => {};
 
+const forgotpassController = (req, res) => {};
 
-
-
-
-
-
-const loginController = (req, res) => {
-
-
-}
-
-
-
-
-const logoutController = (req, res) => {
-
-
-}
-
-
-
-module.exports = { registerController, loginController, logoutController }
+module.exports = {
+  registerController,
+  loginController,
+  logoutController,
+  forgotpassController,
+};
--- index.js
+++ index.js
@@ -1,29 +1,28 @@
-const express =  require ('express')       // importing express from node modules in index.js  which is used for creating server 
-const mongoose = require ('mongoose')   // importing mongoose libarary which is used for connecting mongo db 
-const bodyParser =require('body-parser')
-const {registerController , loginController , logoutController} = require ('./controllers/userController')
+const express = require('express')       // importing express from node modules in index.js  which is used for creating server 
+const mongoose = require('mongoose')     // importing mongoose libarary which is used for connecting mongo db 
+const bodyParser = require('body-parser')
+const { registerController,
+  loginController,
+  logoutController,
+  forgotpassController } = require('./controllers/userController')
 
 
-
-
-
-const app = express()     //  declaring a variable  app in which express function is called 
-// app.use(express.json())   // using express encoding itself 
-app.use(bodyParser.json())   //using third party libarary 
+const app = express()      //  declaring a variable  app in which express function is called 
+// app.use(express.json()) //  using express encoding itself 
+app.use(bodyParser.json()) //  using third party libarary 
 
 
 // configuring env so that  we can save senstive data in protected file 
 require('dotenv').config();
 const Port = process.env.PORT     // declaring a variable and passing a value of from env
-const url = process.env.MONGO_URL    // declaring a variable url and passing a value of from env
+const url = process.env.MONGO_URL // declaring a variable url and passing a value of from env
 
 
 // connecting with  mongo db by using mongoose 
-
-if(mongoose.connect(url)){
-  console.log( `Database connected on ${url}`)
+if (mongoose.connect(url)) {
+  console.log(`Database connected on ${url}`)
 } else {
-  console.log( `Error connecting ${url}`)
+  console.log(`Error connecting ${url}`)
 }
 
 
@@ -33,13 +32,12 @@ if(mongoose.connect(url)){
 
 
 // routes 
+app.get('/', (req, res) => { res.send("helloworld") })
+app.post('/user/register', registerController)
+app.post('/user/login', loginController)
+app.post('/user/logout', logoutController)
+app.post('/user/forgotPassword', forgotpassController)
 
 
-
-app.post('/user/register' , registerController)
-app.post('/user/login' , loginController)
-app.post('/user/logout' , logoutController)
-
-
-  //  starting a server 
-app.listen(Port , console.log( `server conected on localhost : ${Port} `))       //console.log   =>template literal
+//  starting a server      //console.log   =>template literal
+app.listen(Port, console.log(`server conected on localhost : ${Port} `))       
